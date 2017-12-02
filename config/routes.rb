c8479255770554c61ade0912012fff512c85cd06
Anna Borja <aborja@change.org>
2017-12-02 14:32:26 -0800

    Add authentication functionality to the global nav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'account/subscription'
+  get 'account/subscription', as: 'subscription'
 
   root 'home#index'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions]
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: 'new_user_session'
+    get 'logout', to: 'devise/sessions#destroy', as: 'destroy_user_session'
+
+    post 'login', to: 'devise/sessions#create', as: 'user_session'
+  end
 end
