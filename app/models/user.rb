07d9cdcb873f854d9b789e2709456c66c76b2f37
Anna Borja <aborja@change.org>
2017-12-03 05:52:03 -0800

    Style login page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :recoverable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   has_many :memberships
   has_many :membership_levels, through: :memberships
